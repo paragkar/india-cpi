825db147c86a293510a5908377013b8a25b49cc0
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-13 10:20:07 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -51,11 +51,11 @@ df = df.sort_values(by='Date')
 # Convert Date column to string without time
 df['Date_str'] = df['Date'].dt.strftime('31st Mar %Y')
 
-# Format the Value column to two decimal places and keep it as a float
-df['Value'] = df['Value'].astype(float).round(2)
+# # Format the Value column to two decimal places and keep it as a float
+# df['Value'] = df['Value'].astype(float).round(2)
 
-# Create a column to hold the value information along with the year
-df['Text'] = df.apply(lambda row: f"<b>{row['Value']:.2f} ({row['Date_str'][-4:]})</b>", axis=1)
+# # Create a column to hold the value information along with the year
+# df['Text'] = df.apply(lambda row: f"<b>{row['Value']:.2f} ({row['Date_str'][-4:]})</b>", axis=1)
 
 st.write(df)
 
