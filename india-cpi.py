f26bbf3e2681b175c9091834d2d869e7ffa7e0be
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 10:51:39 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -261,7 +261,7 @@ else:
     fig.update_yaxes(categoryorder='array', categoryarray=categories_reversed, row=1, col=2)
 
    # Update the layout for the combined figure
-    fig.update_xaxes(row=1, col=1, range=[overall_min_value, overall_max_value * 1.15], fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
+    fig.update_xaxes(row=1, col=1, range=[overall_min_value, overall_max_value * 1.05], fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
     fig.update_yaxes(row=1, col=1, fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
 
     if selected_metric_type == "Inflation":
