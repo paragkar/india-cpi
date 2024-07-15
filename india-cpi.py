5c20495d4405057c08c974ef6584a48a8597b75e
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 09:06:47 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -256,3 +256,5 @@ else:
     fig.update_layout(height=700, width=1200, margin=dict(l=0, r=10, t=0, b=20, pad=0), showlegend=False)
 
     st.plotly_chart(fig, use_container_width=True)
+
+    st.markdown(f"<h1 style='font-size:40px; margin-top: -40px;'>{"This is CPI"}</h1>", unsafe_allow_html=True)
