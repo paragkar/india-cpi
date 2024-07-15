b5b7c3c1ab49505e9d8db22d9961e4722b70ce18
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 09:11:23 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -238,7 +238,8 @@ else:
 
     # Adjust x-axis range for scatter plot
     max_value = df_filtered_date['Value'].max()
-    fig.update_layout(xaxis=dict(range=[0, max_value * 1.2]))
+    min_value = df_filtered_date['Value'].min()
+    fig.update_layout(xaxis=dict(range=[min_value, max_value * 1.2]))
 
     scatter_fig.update_layout(showlegend=False, xaxis_title="Value of " + selected_metric_type)
     bar_fig.update_layout(showlegend=False, xaxis_title="Weighted Average", yaxis=dict(showticklabels=False))
