5ab9a1f15fa2912c5952b24fce22ef8baaf300e7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 18:54:28 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -316,7 +316,7 @@ else:
 
     fig.update_yaxes(row=1, col=2, fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
     
-    fig.update_layout(height=700, width=1200, margin=dict(l=5, r=10, t=10, b=10, pad=0), showlegend=False)
+    fig.update_layout(height=700, width=1200, margin=dict(l=5, r=10, t=0, b=0, pad=0), showlegend=False)
 
      # Update the layout for the combined figure with x-axis labels
     fig.update_xaxes(title_text="CPI " + selected_metric_type, row=1, col=1, title_font=dict(size=15, family='Arial', color='black', weight='bold'))
