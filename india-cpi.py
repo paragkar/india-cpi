83dd236364e60269f5d8cea0e0de2194a57ea54c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 08:29:14 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -233,7 +233,7 @@ else:
     bar_fig = px.bar(df_filtered_date, x="Weighted Average", y="Description", orientation='h', text_auto='.2f')
 
     scatter_fig.update_traces(marker=dict(line=dict(width=2, color='black')), textposition='middle right', textfont=dict(size=16))
-    bar_fig.update_traces(textposition='outside', textfont=dict(size=12), marker=dict(line=dict(width=2, color='black')))
+    bar_fig.update_traces(textposition='auto', textfont=dict(size=12), marker=dict(line=dict(width=2, color='black')))
 
     scatter_fig.update_layout(showlegend=True, xaxis_title="Value of " + selected_metric_type)
     bar_fig.update_layout(showlegend=False, xaxis_title="Weighted Average", yaxis=dict(showticklabels=False))
@@ -244,6 +244,6 @@ else:
     for trace in bar_fig.data:
         fig.add_trace(trace, row=1, col=2)
 
-    fig.update_layout(height=700, width=1200, margin=dict(l=0, r=20, t=0, b=40, pad=0),showlegend=False)
+    fig.update_layout(height=700, width=1200, margin=dict(l=0, r=10, t=0, b=40, pad=0),showlegend=False)
 
     st.plotly_chart(fig, use_container_width=True)
