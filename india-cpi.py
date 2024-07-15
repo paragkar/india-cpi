e4ffe151f6935cb3d12a1870efabebec440fa191
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 16:16:18 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -278,15 +278,28 @@ else:
 
     fig = make_subplots(rows=1, cols=2, shared_yaxes=True, column_widths=[0.8, 0.2], horizontal_spacing=0.01)  # Minimal horizontal spacing
 
-    # scatter_fig = px.scatter(df_filtered_date, x="Value", y="Description", color="Description", size="Weight", size_max=20, text="Text")
+    # # scatter_fig = px.scatter(df_filtered_date, x="Value", y="Description", color="Description", size="Weight", size_max=20, text="Text")
+    # scatter_fig = px.scatter(df_filtered_date, x="Value", y="Description", color="Description", size_max=20, text="Text")
+    # scatter_fig.update_traces(marker=dict(size=15))  # Adjust the size value as needed
+    # scatter_fig.update_traces(marker=dict(line=dict(width=1, color='black')), textposition='middle right', textfont=dict(family='Arial',size=15, color= 'black',weight='bold'))
+    # scatter_fig.update_layout(showlegend=False, xaxis_title="Value of " + selected_metric_type)
+
+    # bar_fig = px.bar(df_filtered_date, x="Weighted Average", y="Description", orientation='h', text_auto='.1f')
+    # bar_fig.update_traces(textposition='outside', textfont=dict(size=15, family='Arial', color='black', weight='bold'), marker=dict(color='red', line=dict(width=2, color='red')))
+    # bar_fig.update_layout(showlegend=False, xaxis_title="Weighted Average", yaxis=dict(showticklabels=False))
+
+
+    # Create scatter plot
     scatter_fig = px.scatter(df_filtered_date, x="Value", y="Description", color="Description", size_max=20, text="Text")
     scatter_fig.update_traces(marker=dict(size=15))  # Adjust the size value as needed
-    scatter_fig.update_traces(marker=dict(line=dict(width=1, color='black')), textposition='middle right', textfont=dict(family='Arial',size=15, color= 'black',weight='bold'))
+    scatter_fig.update_traces(marker=dict(line=dict(width=1, color='black')), textposition='middle right', textfont=dict(family='Arial', size=15, color='black', weight='bold'))
     scatter_fig.update_layout(showlegend=False, xaxis_title="Value of " + selected_metric_type)
 
-    bar_fig = px.bar(df_filtered_date, x="Weighted Average", y="Description", orientation='h', text_auto='.1f')
-    bar_fig.update_traces(textposition='outside', textfont=dict(size=15, family='Arial', color='black', weight='bold'), marker=dict(color='red', line=dict(width=2, color='red')))
+    # Create bar plot with specific colors
+    bar_fig = px.bar(df_filtered_date, x="Weighted Average", y="Description", color="Description", orientation='h', text_auto='.1f')
+    bar_fig.update_traces(textposition='outside', textfont=dict(size=15, family='Arial', color='black', weight='bold'), marker=dict(line=dict(width=2, color='black')))
     bar_fig.update_layout(showlegend=False, xaxis_title="Weighted Average", yaxis=dict(showticklabels=False))
+
 
     for trace in scatter_fig.data:
         fig.add_trace(trace, row=1, col=1)
