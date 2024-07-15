377d69bee9ae4ef4377648e1b68f2e2789db4668
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 10:07:19 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -271,7 +271,7 @@ else:
     fig.update_xaxes(row=1, col=2, range=[0, max_weighted_avg * 1.3],fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
     fig.update_yaxes(row=1, col=2, fixedrange=True, showline=True, linewidth=1.5, linecolor='grey', mirror=True, showgrid=True, gridcolor='lightgrey')
     
-    fig.update_layout(height=700, width=1200, margin=dict(l=0, r=10, t=0, b=10, pad=0), showlegend=False)
+    fig.update_layout(height=700, width=1200, margin=dict(l=5, r=10, t=0, b=10, pad=0), showlegend=False)
 
     title = "This is CPI"
 
