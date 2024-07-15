2f1f4946dd6683d39be77d612153860336ea870f
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-15 15:45:21 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -283,7 +283,7 @@ else:
 
     scatter_fig.update_traces(marker=dict(line=dict(width=2, color='black')), textposition='middle right', textfont=dict(size=16))
 
-    bar_fig.update_traces(textposition='outside', textfont=dict(size=15, family='Arial', color='black', weight='bold'), marker=dict(color='red', line=dict(width=2, color='black')))
+    bar_fig.update_traces(textposition='outside', textfont=dict(size=15, family='Arial', color='black', weight='bold'), marker=dict(color='red', line=dict(width=2, color='red')))
 
     scatter_fig.update_layout(showlegend=False, xaxis_title="Value of " + selected_metric_type)
 
