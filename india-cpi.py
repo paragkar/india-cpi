6c1805e2e070cecb47e98c7afb3e288172db66fe
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-16 06:57:56 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -340,8 +340,9 @@ else:
     # Create the styled title
     styled_category_type = f"<span style='color:red; font-weight:bold;'>{selected_category_type}</span>"
     styled_sector_type = f"<span style='color:blue; font-weight:bold;'>{selected_sector_type}</span>"
+    styled_metric_type = f"<span style='color:brown; font-weight:bold;'>{selected_metric_type}</span>"
     styled_month = f"<span style='color:green; font-weight:bold;'>{selected_date.strftime('%b %Y')}</span>"
-    title = f"Consumer Price {styled_category_type} {styled_sector_type} {selected_metric_type} Data For Month - {styled_month}"
+    title = f"Consumer Price {styled_category_type} {styled_sector_type} {styled_metric_type} Data For Month - {styled_month}"
 
     # Display the date with month on top along with the title
     st.markdown(f"<h1 style='font-size:30px; margin-top: -20px;'>{title}</h1>", unsafe_allow_html=True)
