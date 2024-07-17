f4afaad4fa972d67e0c285599f26fa25f1668f89
Parag Kar <40283772+paragkar@users.noreply.github.com>
2024-07-17 08:39:34 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -350,16 +350,20 @@ else:
 
     # Initialize title and slider
     update_title(unique_dates[0])
-    slider = slider_placeholder.slider("Slider for Selecting Date Index", min_value=0, max_value=len(unique_dates) - 1, value=0, key="date_slider")
+    slider_label = f"Slider for Selecting Date Index ({unique_dates[0].strftime('%b %Y')})"
+    slider = slider_placeholder.slider(slider_label, min_value=0, max_value=len(unique_dates) - 1, value=0, key="date_slider")
 
     if play_button:
         for i in range(len(unique_dates)):
             selected_date = unique_dates[i]
             update_plot(selected_date)
             update_title(selected_date)
-            slider_placeholder.slider("Slider for Selecting Date Index", min_value=0, max_value=len(unique_dates) - 1, value=i, key=f"date_slider_{i}")
-            time.sleep(0.4)  # Adjust the sleep time to control the animation speed
+            slider_label = f"Slider for Selecting Date Index ({unique_dates[i].strftime('%b %Y')})"
+            slider_placeholder.slider(slider_label, min_value=0, max_value=len(unique_dates) - 1, value=i, key=f"date_slider_play_{i}")
+            time.sleep(0.5)  # Adjust the sleep time to control the animation speed
     else:
         selected_date = unique_dates[slider]
         update_plot(selected_date)
         update_title(selected_date)
+        slider_label = f"Slider for Selecting Date Index ({selected_date.strftime('%b %Y')})"
+        slider_placeholder.slider(slider_label, min_value=0, max_value=len(unique_dates) - 1, value=slider, key="date_slider")
