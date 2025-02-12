c3ee8cc25b416830229dc1a3d04b9b794e87632c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2025-02-12 14:24:14 +0530

    Update india-cpi.py

--- india-cpi.py
+++ india-cpi.py
@@ -356,7 +356,9 @@ else:
         # Display the plot in the placeholder
         # Display the plot in the placeholder
         # plot_placeholder.plotly_chart(fig, use_container_width=True, key=f"plot_{selected_date}")
-        plot_placeholder.plotly_chart(fig, use_container_width=True)
+        # plot_placeholder.plotly_chart(fig, use_container_width=True)
+        plot_placeholder.plotly_chart(fig, use_container_width=True, key=f"plot_{selected_date}")
+
 
 
     def update_title(selected_date):
